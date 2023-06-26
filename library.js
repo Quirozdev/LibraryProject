6bd3a25f593bcbf8290ad27ef62ac6d088bdd16d
Luis <luisdevv232@gmail.com>
2023-06-25 20:57:23 -0700

    Change from constructor functions to classes

--- library.js
+++ library.js
@@ -7,116 +7,127 @@ const addBookBtn = document.querySelector("#add-book-btn");
 const booksContainer = document.querySelector(".books-container");
 
 
-function Library() {
-    this.books = [];
-};
-
-Library.prototype.addBook = function(book) {
-    this.books.push(book);
-};
-
-Library.prototype.displayBooks = function() {
-    // to avoid that the container repeat its children
-    booksContainer.replaceChildren();
-    // if there are books
-    if (this.books.length > 0) {
-        this.books.forEach((book, index) => {
-            const bookCard = book.createDomCard(index);
-            booksContainer.appendChild(bookCard);
-        });
-    } else {
-        const advice = document.createElement("h2");
-        advice.textContent = "There are no books, try adding some!";
-        advice.classList.add("advice");
-        booksContainer.appendChild(advice);
+class Library {
+    constructor() {
+        this._books = []; 
     }
+
+    get books() {
+        return this._books;
+    }
+
+    addBook(book) {
+        this._books.push(book);
+    }
+
+    displayBooks() {
+        // to avoid that the container repeat its children
+        booksContainer.replaceChildren();
+        // if there are books
+        if (this.books.length > 0) {
+            this.books.forEach((book, index) => {
+                const bookCard = book.createDomCard(index);
+                booksContainer.appendChild(bookCard);
+            });
+        } else {
+            const advice = document.createElement("h2");
+            advice.textContent = "There are no books, try adding some!";
+            advice.classList.add("advice");
+            booksContainer.appendChild(advice);
+        }
+    }
+    
 };
 
-function Book(title, author, pages, imageUrl, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.imageUrl = imageUrl;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, imageUrl, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.imageUrl = imageUrl;
+        this.read = read;
+    }
+
+    toggleReadStatus() {
+        this.read = !this.read;
+    }
+
+    createDomCard(index) {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+        // data-attribute represents the index of the book in the array of books of the Library object
+        bookCard.setAttribute("data-attribute", index);
+    
+        const bookImage = document.createElement("img");
+        bookImage.src = this.imageUrl;
+        bookImage.alt = `Book: ${this.title}`;
+        bookImage.classList.add("book-image");
+    
+        const bookTitle = document.createElement("p");
+        bookTitle.textContent = this.title;
+        bookTitle.classList.add("book-title");
+    
+        const bookAuthor = document.createElement("p");
+        bookAuthor.textContent = this.author;
+        bookAuthor.classList.add("book-author");
+    
+        const pagesNumber = document.createElement("p");
+        pagesNumber.classList.add("book-pages");
+        pagesNumber.textContent = this.pages;
+    
+        const bookReadBtn = document.createElement("button");
+        const isBookAlreadyRead = this.read;
+        bookReadBtn.textContent = isBookAlreadyRead ? "Book read" : "Book not read yet";
+        bookReadBtn.classList.add("book-read-btn");
+    
+        if (isBookAlreadyRead) {
+            bookReadBtn.classList.add("read");
+        }
+    
+        bookReadBtn.addEventListener("click", (e) => {
+            // bookCard is the parent of the parent (bookBtnsContainer)
+            // and it holds the data-attribute
+            const bookIndex = Number(e.target.parentElement.parentElement.getAttribute("data-attribute"));
+            const book = myLibrary.books[bookIndex];
+            // toggle read status, invert message
+            book.toggleReadStatus();
+            if (book.read) {
+                bookReadBtn.textContent = "Book read";
+                bookReadBtn.classList.add("read");
+            } else {
+                bookReadBtn.textContent = "Book not read yet";
+                bookReadBtn.classList.remove("read");
+            }
+        });
+    
+        const deleteBookBtn = document.createElement("button");
+        deleteBookBtn.textContent = "Delete";
+        deleteBookBtn.setAttribute("data-attribute", index);
+        deleteBookBtn.classList.add("delete-book-btn");
+    
+        deleteBookBtn.addEventListener("click", (e) => {
+            const bookIndex = Number(e.target.parentElement.parentElement.getAttribute("data-attribute"));
+            myLibrary.books.splice(bookIndex, 1);
+            myLibrary.displayBooks();
+        });
+    
+        const bookBtnsContainer = document.createElement("div");
+        bookBtnsContainer.classList.add("book-buttons-container");
+    
+        bookBtnsContainer.appendChild(bookReadBtn);
+        bookBtnsContainer.appendChild(deleteBookBtn);
+    
+        bookCard.appendChild(bookImage);
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(pagesNumber);
+        bookCard.appendChild(bookBtnsContainer);
+    
+        return bookCard;
+    }
+    
 }
 
-Book.prototype.toggleReadStatus = function() {
-    this.read = !this.read;
-};
-
-Book.prototype.createDomCard = function(index) {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("book-card");
-    // data-attribute represents the index of the book in the array of books of the Library object
-    bookCard.setAttribute("data-attribute", index);
-
-    const bookImage = document.createElement("img");
-    bookImage.src = this.imageUrl;
-    bookImage.alt = `Book: ${this.title}`;
-    bookImage.classList.add("book-image");
-
-    const bookTitle = document.createElement("p");
-    bookTitle.textContent = this.title;
-    bookTitle.classList.add("book-title");
-
-    const bookAuthor = document.createElement("p");
-    bookAuthor.textContent = this.author;
-    bookAuthor.classList.add("book-author");
-
-    const pagesNumber = document.createElement("p");
-    pagesNumber.classList.add("book-pages");
-    pagesNumber.textContent = this.pages;
-
-    const bookReadBtn = document.createElement("button");
-    const isBookAlreadyRead = this.read;
-    bookReadBtn.textContent = isBookAlreadyRead ? "Book read" : "Book not read yet";
-    bookReadBtn.classList.add("book-read-btn");
-
-    if (isBookAlreadyRead) {
-        bookReadBtn.classList.add("read");
-    }
-
-    bookReadBtn.addEventListener("click", (e) => {
-        // bookCard is the parent of the parent (bookBtnsContainer)
-        // and it holds the data-attribute
-        const bookIndex = Number(e.target.parentElement.parentElement.getAttribute("data-attribute"));
-        const book = myLibrary.books[bookIndex];
-        // toggle read status, invert message
-        book.toggleReadStatus();
-        if (book.read) {
-            bookReadBtn.textContent = "Book read";
-            bookReadBtn.classList.add("read");
-        } else {
-            bookReadBtn.textContent = "Book not read yet";
-            bookReadBtn.classList.remove("read");
-        }
-    });
-
-    const deleteBookBtn = document.createElement("button");
-    deleteBookBtn.textContent = "Delete";
-    deleteBookBtn.setAttribute("data-attribute", index);
-    deleteBookBtn.classList.add("delete-book-btn");
-
-    deleteBookBtn.addEventListener("click", (e) => {
-        const bookIndex = Number(e.target.parentElement.parentElement.getAttribute("data-attribute"));
-        myLibrary.books.splice(bookIndex, 1);
-        myLibrary.displayBooks();
-    });
-
-    const bookBtnsContainer = document.createElement("div");
-    bookBtnsContainer.classList.add("book-buttons-container");
-
-    bookBtnsContainer.appendChild(bookReadBtn);
-    bookBtnsContainer.appendChild(deleteBookBtn);
-
-    bookCard.appendChild(bookImage);
-    bookCard.appendChild(bookTitle);
-    bookCard.appendChild(bookAuthor);
-    bookCard.appendChild(pagesNumber);
-    bookCard.appendChild(bookBtnsContainer);
-
-    return bookCard;
-};
 
 const theHobbitBook = new Book(
     "The Hobbit",
